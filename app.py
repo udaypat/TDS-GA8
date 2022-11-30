39406f6828c09081f9004f4a91f1d9b0d2df0919
Uday Patil <115341269+udaypat@users.noreply.github.com>
2022-11-30 23:10:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
+import numpy as np
+import pandas as pd
 import streamlit as st
 
-st.title("Division of Two Numbers")
-html_temp = """
-<div style="background-color:black;padding:10px">
-<h2 style="color:black;text-align:center;">Division of 2 Numbers</h2>
-</div>
-"""
-st.markdown(html_temp,unsafe_allow_html=True)
-num1 = st.number_input("Number 1")
-num2 = st.number_input("Number 2")
 
-if(num1==0 or num2==0):
-  result = 0
-else:
-  result=num1/num2
-
-st.success('The output is {}'.format(result))
-
+def main():
+  st.title("Division of Two Numbers")
+  html_temp = """
+  <div style="background-color:black;padding:10px">
+  <h2 style="color:black;text-align:center;">Division of 2 Numbers</h2>
+  </div>
+  """
+  st.markdown(html_temp,unsafe_allow_html=True)
+  num1 = st.number_input("Number 1")
+  num2 = st.number_input("Number 2")
+  
+  if(num1==0 or num2==0):
+    result = 0
+  else:
+    result=num1/num2
+  
+  st.success('The output is {}'.format(result))
+  
+if __name__=='__main__':
+  main()
